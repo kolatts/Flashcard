752ca4412e22f96596f11bd8aa923ab5dbe7af97
SJKADMIN <SJKADMIN@CD2UA5472N03>
2016-04-07 15:54:30 -0400

    clockview added

--- Flashcard.Model/ClockQuestion.cs
+++ Flashcard.Model/ClockQuestion.cs
@@ -6,7 +6,11 @@ namespace Flashcard.Model
     {
         public override string Text => "What is the time?";
         public override string CorrectAnswer => $"{Hour}:{Minutes}";
+        public float HourAngle => Hour * 30 * 0.5f * Minutes;
+        public float MinuteAngle => 6 * Minutes;
 
+        public float HourAngleInRadians => (float)Math.PI * HourAngle / 180;
+        public float MinuteAngleInRadians => (float)Math.PI * HourAngle / 180;
         public int Hour { get; set; }
 
         public int Minutes { get; set; }
@@ -29,22 +33,25 @@ namespace Flashcard.Model
             Minutes = GetMinutes();
         }
 
-        protected  int GetMinutes()
+        protected int GetMinutes()
         {
             var rand = RandomSingleton.Instance.Random;
             switch (MinimumInterval)
             {
                 case Interval.FiveMinutes:
-                    return 5 * rand.Next(0,11);
+                    return 5 * rand.Next(0, 11);
+
                 case Interval.FifteenMinutes:
-                    return 15 * rand.Next(0,3);
+                    return 15 * rand.Next(0, 3);
+
                 case Interval.ThirtyMinutes:
-                    return 30 * rand.Next(0,1);
+                    return 30 * rand.Next(0, 1);
+
                 case Interval.SixtyMinutes:
                     return 0;
+
                 default: throw new ArgumentException($"Unknown {nameof(Interval)} : {MinimumInterval}");
             }
         }
-
     }
 }
